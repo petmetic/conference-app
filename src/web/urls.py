4fb03c43da05d3e950ba95411eb5b706e120fe65
Meta <petmetic@gmail.com>
2024-02-27 12:30:27 +0100

    added attendee detail view and edit view

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("atendee/add", views.attendee_add, name="attendee_add"),
     path("atendee/list", views.attendee_list, name="attendee_list"),
+    path("atendee/<int:pk>/detail/", views.attendee, name="attendee"),
+    path("atendee/<int:pk>/detail/edit", views.attendee_edit, name="attendee_edit"),
+    path("atendee/add", views.attendee_add, name="attendee_add"),
     path("arrivals/check", views.arrivals_check, name="arrivals_check"),
     path("arrivals/add", views.arrivals_add, name="arrivals_add"),
 ]
--- src/web/views.py
+++ src/web/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Attendee
 
 
 def index(request):
@@ -9,8 +11,20 @@ def attendee_add(request):
     return render(request, "web/attendee_add.html", {})
 
 
+def attendee(request, pk: int):
+    attendee = get_object_or_404(Attendee, pk=pk)
+    return render(request, "web/attendee.html", {"attendee": attendee})
+
+
+def attendee_edit(request, pk: int):
+    attendee = get_object_or_404(Attendee, pk=pk)
+
+    return render(request, "web/attendee_edit.html", {"attendee": attendee})
+
+
 def attendee_list(request):
-    return render(request, "web/attendee_list.html", {})
+    attendees = Attendee.objects.all().order_by("surname")
+    return render(request, "web/attendee_list.html", {"attendee_list": attendees})
 
 
 def arrivals_check(request):
