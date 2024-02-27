0338021cfb2c4bfe2c4758cefa0cf40cdd726771
Meta <petmetic@gmail.com>
2024-02-27 17:01:21 +0100

    added add-attendee form

--- src/web/forms.py
+++ src/web/forms.py
@@ -49,3 +49,69 @@ class AttendeeEditForm(ModelForm):
                 "Customer already exists in database. Please check for correct input in fields."
             )
         return cleaned_data
+
+
+class AttendeeForm(ModelForm):
+    name = forms.CharField(
+        label="Name",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "type": "text",
+                "placeholder": "enter NAME of atendee",
+            }
+        ),
+    )
+    surname = forms.CharField(
+        label="Surname",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "type": "text",
+                "placeholder": "enter SURNAME of attendee",
+            }
+        ),
+    )
+    birth_date = forms.DateField(
+        label="Date of Birth",
+        label_suffix="",
+        widget=forms.DateTimeInput(
+            format="%Y-%m-%d",
+            attrs={
+                "class": "form-control fw-light",
+                "type": "date",
+            },
+        ),
+    )
+    ticket_id = forms.CharField(
+        label="Ticket ID",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "type": "text",
+                "placeholder": "enter ID number of ticket",
+            }
+        ),
+    )
+
+    class Meta:
+        model = Attendee
+        fields = ["name", "surname", "birth_date", "ticket_id"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data["name"]
+        surname = cleaned_data["surname"]
+        birth_date = cleaned_data["birth_date"]
+        ticket_id = cleaned_data["ticket_id"]
+        attendee = Attendee.objects.filter(
+            name=name, surname=surname, birth_date=birth_date, ticket_id=ticket_id
+        ).first()
+        if attendee:
+            raise ValidationError(
+                "Customer already exists in database. Please check for correct input in fields."
+            )
+        return cleaned_data
