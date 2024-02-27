e4db8910221398542dde2bfb5cb1c0f5d1bf0923
Meta <petmetic@gmail.com>
2024-02-27 10:56:01 +0100

    initial db models for Attendees and Arrivals

--- src/web/admin.py
+++ src/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Attendee, Arrival
 
-# Register your models here.
+
+@admin.register(Arrival)
+class ArrivalAdmin(admin.ModelAdmin):
+    list_filter = ["arrival"]
+    list_display = ["arrival", "attendee"]
+
+
+@admin.register(Attendee)
+class AttendeeAdmin(admin.ModelAdmin):
+    list_filter = ["ticket_id"]
+    list_display = ["name", "surname", "birth_date", "ticket_id"]
