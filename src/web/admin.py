a237a5e1176cdff0a95a01e6d196d2e3539b0858
Meta <petmetic@gmail.com>
2024-03-04 14:26:14 +0100

    Admin search view for Arrival and Attendee models

--- src/web/admin.py
+++ src/web/admin.py
@@ -6,10 +6,20 @@ from simple_history.admin import SimpleHistoryAdmin
 @admin.register(Arrival)
 class ArrivalAdmin(SimpleHistoryAdmin):
     list_filter = ["arrival"]
-    list_display = ["arrival", "attendee"]
+    list_display = ["attendee", "arrival"]
+
+    search_fields = [
+        "attendee__name",
+        "attendee__surname",
+        "attendee__ticket_id",
+        "arrival",
+    ]
+    search_help_text = "Search here using Name, Surname, Ticket ID or arrival date in following format: YYYY-MM-DD"
 
 
 @admin.register(Attendee)
 class AttendeeAdmin(SimpleHistoryAdmin):
     list_filter = ["ticket_id"]
-    list_display = ["name", "surname", "birth_date", "ticket_id"]
+    list_display = ["surname", "name", "birth_date", "ticket_id"]
+    search_fields = ["surname", "name", "ticket_id"]
+    search_help_text = "Search here using Name, Surname, or Ticket ID"
