cae8491fb443e6fc67d1df807908fc5fa3cce6fb
Meta <petmetic@gmail.com>
2024-03-04 11:59:05 +0100

    fixed response status code for POST arrival

--- src/api/tests/test_api.py
+++ src/api/tests/test_api.py
@@ -1,20 +1,26 @@
 import json
+from datetime import datetime
+import pytz
+
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from web.tests.factories import UserFactory, AttendeeFactory, ArrivalFactory
 
 
+tz = pytz.timezone("Europe/Ljubljana")
+
+
 class TestAttendeeAPIView(APITestCase):
     def setUp(self):
-        self.url = reverse("attendee-list")
+        self.url_attendee = reverse("attendee-list")
+        self.url_arrival = reverse("arrivals_add")
         self.user = UserFactory()
         self.attendee = AttendeeFactory()
-        self.arrival = ArrivalFactory(attendee=self.attendee)
         self.client.force_login(self.user)
 
     def test_get_attendees(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.url_attendee)
         response.render()
         self.assertEquals(response.status_code, 200)
 
@@ -35,3 +41,39 @@ class TestAttendeeAPIView(APITestCase):
 
         self.assertEqual(ticket_id_expected, ticket_id_api)
         self.assertEqual(name_expected, name_api)
+
+
+class TestArrivalAPIView(APITestCase):
+    def setUp(self):
+        self.url_arrival = reverse("arrivals_add")
+        self.user = UserFactory()
+        self.attendee = AttendeeFactory()
+        self.client.force_login(self.user)
+
+    def test_post_arrival(self):
+
+        response = self.client.get(self.url_arrival)
+        self.assertEquals(response.status_code, 200)
+
+        csrftoken = response.cookies["csrftoken"]
+
+        arrival = ArrivalFactory(
+            attendee=self.attendee,
+            arrival=datetime(2023, 8, 2, 18, 0, 0).astimezone(tz=tz),
+        )
+        data = {
+            "attendee": arrival.attendee.id,
+            "arrival": arrival.arrival.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+
+        expected_data = {
+            "attendee": "http://127.0.0.1:8000/api/attendee/1/",
+            "arrival": "2023-08-02 18:00:00",
+        }
+
+        resp = self.client.post(
+            self.url_arrival, data, headers={"X=csrftoken": csrftoken}
+        )
+        print(resp.content)
+
+        self.assertEqual(resp.status_code, 200)
