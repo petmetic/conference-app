12856e93c9526ebbb5dd2ec0a60398d8b95f28ea
Meta <petmetic@gmail.com>
2024-03-05 09:02:00 +0100

    added Celery instance and __init__ file so Celery runs  at start of app

--- src/conferenceapp/celery.py
+++ src/conferenceapp/celery.py
@@ -1,16 +1,22 @@
+import os
+
 from celery import Celery
 
-app = Celery(
-    "conferenceapp",
-    broker="redis://",
-    backend="rpc://",
-    include=["conferenceapp.tasks"],
-)
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conferenceapp.settings")
 
-# # Optional configuration, see the application user guide.
-# app.conf.update(
-#     result_expires=3600,
-# )
+app = Celery("conferenceapp")
 
-if __name__ == "__main__":
-    app.start()
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")
