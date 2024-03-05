03e03293c0b9399de5e2708e2814d5a0b60ef866
Meta <petmetic@gmail.com>
2024-03-05 09:35:49 +0100

    adding redis conf to ; might not work because of

--- src/conferenceapp/settings.py
+++ src/conferenceapp/settings.py
@@ -130,3 +130,10 @@ STATIC_ROOT = BASE_DIR / "static"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
+
+
+# REDIS configuration
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
